1d7690b5af3c2bd96bc26e03f2bad87bace4ceed
Jerry Wang <x.jerry.wang@gmail.com>
2024-05-15 22:16:00 +0800

    perf: upgrade deps

--- src/load-pkg.test.ts
+++ src/load-pkg.test.ts
@@ -1,16 +1,17 @@
 import path from 'path'
-import { loadPkg } from '../src'
+import { loadPkg } from './index.js'
 
 describe('load-pkg', () => {
+  const dir = path.resolve('example')
   it('should return a monorepo', async () => {
-    const file = await loadPkg(path.join(__dirname, 'packages/pkg1'))
+    const file = await loadPkg(path.join(dir, 'packages/pkg1'))
 
     expect(file?.monorepo).toBe(false)
     expect(file?.parent?.monorepo).toBe(true)
   })
 
   it('should return a non-monorepo', async () => {
-    const file = await loadPkg(path.join(__dirname, '..'))
+    const file = await loadPkg(path.join(dir, '..'))
 
     expect(file?.monorepo).toBe(false)
   })
